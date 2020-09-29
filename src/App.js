6131a3ed3263b0b22d753ae0d250ca17563b347f
RaySunwar <ray@whitehatagency.com.au>
2020-09-29 11:30:16 +0545

    refactor: fetch data from github api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import useFetchJobs from './components/api/useFetchJobs';
+import { Container } from "react-bootstrap";
 
 function App() {
+  const { jobs, loading, error } = useFetchJobs();
   return (
-    <div className="App">
-    </div>
+    <Container className="App">
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error 404. Try Refreshing.</h1>}
+      <h1>{jobs.length}</h1>
+    </Container>
   );
 }
 
---
+++ src/components/api/useFetchJobs.js
@@ -0,0 +1,45 @@
+import {useReducer, useEffect} from "react";
+import axios from "axios";
+
+const MAIN_URL = "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
+
+const ACTIONS = {
+    MAKE_REQUEST: "make-request",
+    GET_DATA: "get-data",
+    ERROR: "error"
+};
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case ACTIONS.MAKE_REQUEST:
+            return { loading: true, jobs: [] }
+        case ACTIONS.GET_DATA:
+            return {...state, loading: false, jobs: action.payload.jobs }           
+        case ACTIONS.ERROR:
+            return { ...state, loading: false, error: action.payload.error, jobs: []}
+        
+        default:
+            return state
+    };
+};
+
+const useFetchJobs = (params, page) => {
+    const [state, dispatch] = useReducer(reducer, {jobs: [], loading: true });
+
+    useEffect(() =>{
+        dispatch({ type: ACTIONS.MAKE_REQUEST });
+        axios.get(MAIN_URL, {
+            params: { markdown: true, page:page, ...params}
+        })
+        .then(res => {
+            dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data} })
+        })
+        .catch(e => {
+            dispatch({ type: ACTIONS.ERROR, payload: { error: e }})
+        })
+    }, [params, page])  
+
+    return state
+}
+
+export default useFetchJobs;
