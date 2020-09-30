cd1168d5506e8bb7107570cca36725c778d7bca2
RaySunwar <ray@whitehatagency.com.au>
2020-09-30 10:37:46 +0545

    build: add job file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GoMarkGithub } from "react-icons/go"
 import useFetchJobs from './components/api/useFetchJobs';
+import Job from "./components/Job/job";
 import { Container } from "react-bootstrap";
 
 function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
   const { jobs, loading, error } = useFetchJobs();
+
   return (
-    <Container className="App">
+    <Container className="my-5">
+      <h1 className="text-center"> <GoMarkGithub /> Jobs</h1>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error 404. Try Refreshing.</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map(job=> {
+        return <Job key={job.id} job={job} />
+      })}
     </Container>
-  );
-}
+  );  
+};
 
 export default App;
